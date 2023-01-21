01d3a4bda2953e86355c3533e946f72a0caa4cbc
Dmitrii <dimau777@gmail.com>
2023-01-21 17:13:58 +0400

    Section Blog

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.swiper-button-right',
+    prevEl: '.swiper-button-left',
+  },
+});
