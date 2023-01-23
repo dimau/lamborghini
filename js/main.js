2eb87e03c0cc6d8ab75cfc6100639c55cd049c5e
Dmitrii <dimau777@gmail.com>
2023-01-23 18:27:56 +0400

    Adaptive

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 /*** Initializing of slider ***/
 const swiper = new Swiper('.swiper', {
   loop: true,
-  slidesPerView: 2,
-  spaceBetween: 20,
   navigation: {
     nextEl: '.swiper-button-right',
     prevEl: '.swiper-button-left',
   },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    }
+  }
 });
 
 /*** Using Google API for the map in the footer ***/
@@ -145,3 +152,9 @@ function initMap() {
 }
 
 window.initMap = initMap;
+
+const menuBtn = document.querySelector(".menu-btn");
+const menuMobile = document.querySelector(".menu-mobile");
+menuBtn.addEventListener("click", () => {
+  menuMobile.classList.toggle("menu--open");
+});
